019fe33c788d879ed9962a13cbb2c8e67b7e9bbb
unknown <HeatwoleJ@A25194.sra.com>
2016-05-25 22:58:43 -0400

    initial checkin

---
+++ server.R
@@ -0,0 +1,69 @@
+require(dplyr)
+require(ggplot2) # Plotting
+allmatches <- read.csv("http://www.cvilletennis.org/catsdb.csv")
+allmatches <- allmatches[ ,1:10]
+allmatches["set1MOV"] <- NA
+allmatches$set1MOV <- abs(allmatches$WinnerSet1 - allmatches$LoserSet1)
+allmatches <- mutate(allmatches, set3Played = (WinnerSet3 > 0))
+maptable = c("7-6", "Ambiguous", "6-4", "6-3", "6-2", "6-1")
+allmatches$set1Score <- maptable[allmatches$set1MOV]
+
+# allmatches <- mutate(allmatches, outcome = detOutcome(WinnerSet3, A.or.B))
+
+for (i in 1:nrow(allmatches)) {
+  if (allmatches[i, "set3Played"] == FALSE) {
+    allmatches[i, "outcome"] <- "Won in 2 sets"
+  }
+  else if (allmatches[i,"A.or.B"] == "A") {
+    allmatches[i, "outcome"] <- "Won in 3 sets"
+  }
+  else {
+    allmatches[i, "outcome"] <- "Lost in 3 sets"
+  }
+  allmatches[i, "year"] <- floor(allmatches[i, "TimeID"]/10)
+  if (allmatches[i, "set1Score"] == "Ambiguous") {
+    if(allmatches[i, "WinnerSet1"] == 5 || allmatches[i, "WinnerSet1"] == 7) {
+      allmatches[i, "set1Score"] = "7-5"
+    } else {
+      allmatches[i, "set1Score"] = "6-4"
+    }
+  }
+}
+
+shinyServer(
+  function(input, output) {
+
+    observeEvent(input$go, {
+      rv <- reactiveValues(graphTitle = c("Match Results for Year ", input$year))
+      rv$graphTitle<- paste("Match Results for First Set Winner in Year ", input$year)
+      if (input$year == "All Years") {
+        matchset = allmatches
+        rv$graphTitle <- paste("Match Results for First Set Winner - All Years")
+      } else {
+        matchset <- filter(allmatches, year==input$year)
+      }
+      output$text1 <- renderText({c(nrow(matchset), " Total Matches")})
+      ttl <- rv$graphTitle
+      output$YearHist <- renderPlot( {
+        ggplot(matchset, aes(x=set1MOV, colour=outcome)) +
+          geom_histogram(fill="white", position="dodge", binwidth = 0.5) +
+          ggtitle(rv$graphTitle) +
+          xlab("1st Set Margin of Victory (Games)") +
+          ylab("Count") +
+          scale_x_continuous(breaks=seq(1,6,1))
+      } )
+      perOutcome <- matchset %>% group_by(set1MOV, outcome) %>% summarise(outcomes = n())
+      colnames(perOutcome) <- c("First Set Margin of Victory", "Outcome", "Count")
+      perOutcome["Percent"] <- NA
+      for(i in 1:6) {
+        subtotal <- perOutcome[3*(i-1)+1,3] + perOutcome[3*(i-1)+2,3] + perOutcome[3*(i-1)+3,3]
+        for (j in 1:3) {
+          perOutcome[3*(i-1)+j,4] <- 100.0 *perOutcome[3*(i-1)+j,3] / subtotal
+        }
+      }
+#      as.double(100.0*perOutcome[,4])
+#      sprintf("%2.1f %%", perOutcome[,4])
+      output$table1 <- renderTable( {perOutcome})
+    } )
+  }
+)
---
+++ ui.R
@@ -0,0 +1,33 @@
+shinyUI(
+  fluidPage(
+    # Application title
+    tags$h2("CATS Tennis - First Set Winner and Match Results"),
+    tags$img(height = 179, width = 921, 
+#             src = "http://www.cvilletennis.org/images/alt-tennis-court_2.gif"),
+             src = "alt-tennis-court_2.gif"),
+    tags$hr(),
+    tags$p("In 2015, a statistical inference project was completed that demonstrated that those players who win the first set in a community tennis match by 4 games or more",
+           "have a statistically more significant probability of winning the overall match when compared to first set winners by smaller margins.",  
+           "Data for this study were collected for community tennis matches completed under the CATS organization. You can vistit the CATS website",
+    tags$a(href = "http://www.cvilletennis.org", "here."),
+        "The study can be reviewed",
+    tags$a(href = "http://www.cvilletennis.org/RecreationalTennis.html", "here.")),
+    tags$p("This Shiny app provides users with a mechanism to visualize this data and judge whether the trend has changed over the organization's ten year history.",
+        "Select a year (or 'All Years') in the drop-down box and click the Submit button. A histogram will be generated showing the frequency of various outcomes for each first set score for all matches played in that time period." ,   
+        "The Shiny app generates a table beneath the histogram for those viewers who desire access to the actual values."),
+    wellPanel(
+      selectInput('year', 'Matches Played In', 
+                  c("All Years",2006,2007,2008,2009,2010,2011,2012,2013,2014,2015),
+                  selected = 2006, width='160px'), 
+      actionButton(inputId = "go", label = "Submit")
+    ),
+    tags$hr(),
+    textOutput('text1'),
+    plotOutput('YearHist'),
+    tags$hr(),
+    tags$img(src = "tennis-ball-banner.gif"),
+    tags$hr(),
+    tableOutput('table1')
+  )
+
+)
