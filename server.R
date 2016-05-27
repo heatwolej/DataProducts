ed12498741e7d6ad0ede8f8eefca8bc0822a35c2
unknown <HeatwoleJ@A25194.sra.com>
2016-05-27 17:57:47 -0400

    Updated Shiny App to include multiple tabs on output. Moved Table Data to "Tabular" tab and added a raw data xtable

--- server.R
+++ server.R
@@ -1,13 +1,15 @@
 require(dplyr)
 require(ggplot2) # Plotting
+require(DT)
+
+## Read prepared csv file from CATS website, add useful columns
 allmatches <- read.csv("http://www.cvilletennis.org/catsdb.csv")
 allmatches <- allmatches[ ,1:10]
 allmatches["set1MOV"] <- NA
 allmatches$set1MOV <- abs(allmatches$WinnerSet1 - allmatches$LoserSet1)
 allmatches <- mutate(allmatches, set3Played = (WinnerSet3 > 0))
-maptable = c("7-6", "Ambiguous", "6-4", "6-3", "6-2", "6-1")
-allmatches$set1Score <- maptable[allmatches$set1MOV]
 
+# set outcome column for histogram display
 for (i in 1:nrow(allmatches)) {
   if (allmatches[i, "set3Played"] == FALSE) {
     allmatches[i, "outcome"] <- "Won in 2 sets"
@@ -19,13 +21,6 @@ for (i in 1:nrow(allmatches)) {
     allmatches[i, "outcome"] <- "Lost in 3 sets"
   }
   allmatches[i, "year"] <- floor(allmatches[i, "TimeID"]/10)
-  if (allmatches[i, "set1Score"] == "Ambiguous") {
-    if(allmatches[i, "WinnerSet1"] == 5 || allmatches[i, "WinnerSet1"] == 7) {
-      allmatches[i, "set1Score"] = "7-5"
-    } else {
-      allmatches[i, "set1Score"] = "6-4"
-    }
-  }
 }
 
 shinyServer(
@@ -34,6 +29,8 @@ shinyServer(
     observeEvent(input$go, {
       rv <- reactiveValues(graphTitle = c("Match Results for Year ", input$year))
       rv$graphTitle<- paste("Match Results for First Set Winner in Year ", input$year)
+      
+      ## set Graph Title and filter Data set
       if (input$year == "All Years") {
         matchset = allmatches
         rv$graphTitle <- paste("Match Results for First Set Winner - All Years")
@@ -41,7 +38,6 @@ shinyServer(
         matchset <- filter(allmatches, year==input$year)
       }
       output$text1 <- renderText({c(nrow(matchset), " Total Matches")})
-      ttl <- rv$graphTitle
       output$YearHist <- renderPlot( {
         ggplot(matchset, aes(x=set1MOV, colour=outcome)) +
           geom_histogram(fill="white", position="dodge", binwidth = 0.5) +
@@ -50,8 +46,11 @@ shinyServer(
           ylab("Count") +
           scale_x_continuous(breaks=seq(1,6,1))
       } )
+      ## prepare subtotals for table display
       perOutcome <- matchset %>% group_by(set1MOV, outcome) %>% summarise(outcomes = n())
       colnames(perOutcome) <- c("First Set Margin of Victory", "Outcome", "Count")
+      
+      ## manually calculate percentage column for table display
       perOutcome["Percent"] <- NA
       for(i in 1:6) {
         subtotal <- perOutcome[3*(i-1)+1,3] + perOutcome[3*(i-1)+2,3] + perOutcome[3*(i-1)+3,3]
@@ -60,6 +59,10 @@ shinyServer(
         }
       }
       output$table1 <- renderTable( {perOutcome})
+      output$table2 <- DT::renderDataTable(
+        DT::datatable(matchset, options = list(pageLength = 25))
+      )    
+      
     } )
   }
 )
--- ui.R
+++ ui.R
@@ -1,17 +1,21 @@
+# source code submitted to github at https://github.com/heatwolej/DataProducts
+# shiny App deployed to https://jheatwole.shinyapps.io/ShinyTennis/
+# R Presentation deployed to http://rpubs.com/heatwolej/183859
+
 shinyUI(
   fluidPage(
     # Application title
-    tags$h2("CATS Tennis - First Set Winner and Match Results"),
-    tags$img(height = 179, width = 921, 
+    h2("CATS Tennis - First Set Winner and Match Results"),
+    img(height = 179, width = 921, 
              src = "alt-tennis-court_2.gif"),
-    tags$hr(),
-    tags$p("In 2015, a statistical inference project was completed that demonstrated that those players who win the first set in a community tennis match by 4 games or more",
+    hr(),
+    p("In 2015, a statistical inference project was completed that demonstrated that those players who win the first set in a community tennis match by 4 games or more",
            "have a statistically more significant probability of winning the overall match when compared to first set winners by smaller margins.",  
            "Data for this study were collected for community tennis matches completed under the CATS organization. You can vistit the CATS website",
-    tags$a(href = "http://www.cvilletennis.org", "here."),
+    a(href = "http://www.cvilletennis.org", "here."),
         "The study can be reviewed",
-    tags$a(href = "http://www.cvilletennis.org/RecreationalTennis.html", "here.")),
-    tags$p("This Shiny app provides users with a mechanism to visualize this data and judge whether the trend has changed over the organization's ten year history.",
+    a(href = "http://www.cvilletennis.org/RecreationalTennis.html", "here.")),
+    p("This Shiny app provides users with a mechanism to visualize this data and judge whether the trend has changed over the organization's ten year history.",
         "Select a year (or 'All Years') in the drop-down box and click the Submit button. A histogram will be generated showing the frequency of various outcomes for each first set score for all matches played in that time period." ,   
         "The Shiny app generates a table beneath the histogram for those viewers who desire access to the actual values."),
     wellPanel(
@@ -20,13 +24,14 @@ shinyUI(
                   selected = 2006, width='160px'), 
       actionButton(inputId = "go", label = "Submit")
     ),
-    tags$hr(),
+    hr(),
     textOutput('text1'),
-    plotOutput('YearHist'),
-    tags$hr(),
-    tags$img(src = "tennis-ball-banner.gif"),
-    tags$hr(),
-    tableOutput('table1')
+    tabsetPanel(type = "tabs",
+                tabPanel("Histogram", plotOutput('YearHist')),
+                tabPanel("Tabular Results", tableOutput('table1')),
+                tabPanel("Raw Data", DT::dataTableOutput('table2'))),
+    hr(),
+    img(src = "tennis-ball-banner.gif")
   )
 
 )
